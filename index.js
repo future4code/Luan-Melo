502724a58cc9b3ff7338c99f7805072741d8ac27
Luan-Melo <luan_decruz19@hotmail.com>
2021-06-14 15:50:27 -0300

    atividade javascript, variáveis

---
+++ index.js
@@ -0,0 +1,84 @@
+// -------------------------------- Exercício ------------------------------------  
+//
+// -------------------------------------------------------------------------------
+// atribui o valor 10 a variável a
+// let a = 10
+// atribui o valor 10 a variável b
+// let b = 10
+// printa no console o valor atribuído a b
+// console.log(b)
+// atribui um novo valor a variável b
+// b = 5
+// retorna o valor de a e de b
+// console.log(a, b)
+
+// -------------------------------- Exercício ------------------------------------
+//
+// -------------------------------------------------------------------------------
+// atribui o valor 10 a variável a
+// let a = 10
+// atribui o valor 20 a variável b
+// let b = 20
+// não atribui nenhum valor a variável c, logo ela recebe um undefined
+// let c
+// a variável c recebe o valor da variável a, logo c passa a ser 10
+// c = a
+// a varável b recebe o valor da variável c, logo ela perde o valor de 20 e passa a ser 10
+// b = c
+// a variável a recebe o valor de b que é 10.
+// a = b
+// printa no console os valores das variáveis, e todas as variáveis vale 10
+// console.log(a, b, c)
+
+// para variável p - let horasPorDia
+// let p = prompt("Quantas horas você trabalha por dia?")
+// para viriável t - let salarioDiario
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// -------------------------------- Exercício ------------------------------------ 
+//
+// -------------------------------------------------------------------------------
+// let nome
+// let idade
+// retorna undefined, pois as variáveis não possuí um valor definido
+// console.log("O tipo da variável nome é :", typeof nome)
+// console.log("O tipo da variável idade é :", typeof idade)
+
+//-------------------------------- Exercício de escrita de código I ------------------------------------
+//
+// -------------------------------------------------------------------------------
+// let nome = prompt('Escreva seu nome: ?')
+// let idade = Number(prompt('Qual a sua idade: ?'))
+// let nome1 = nome
+// let idade1 = idade
+// retorna uma string na variável nome e um number na variável idade
+// console.log("O tipo da variável nome é :", typeof nome1)
+// console.log("O tipo da variável idade é :", typeof idade1)
+// console.log("Olá", nome1, "sua idade é:", idade1)
+//
+//-------------------------------- Exercício de escrita de código II ------------------------------------//
+//                       Construa um programa, seguindo os seguintes passos:                                        
+//-------------------------------------------------------------------------------------------------------//
+// const corRoupa = prompt('Você está usando uma roupa azul hoje: ?')
+// const gostaDeCafe = prompt('você gosta de café? ')
+// const gostaDeAcucar = prompt('você gosta de açúcar? ')
+// const resposta1 = corRoupa
+// const resposta2 = gostaDeCafe
+// const resposta3 = gostaDeAcucar
+// console.log('Você está usando uma roupa azul hoje: ? -', resposta1)
+// console.log('você gosta de café? -', resposta2)
+// console.log('você gosta de açúcar? -', resposta3)
+//
+//-------------------------------- Exercício de escrita de código II ------------------------------------
+//               Suponha que temos duas variáveis a e b, cada uma com um valor inicial
+//-------------------------------------------------------------------------------------------------------
+//
+// let a = 10
+// let b = 25
+// let c = a
+// a = b
+// b = c
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+//-------------------------------------------------------------------------------------------------------
