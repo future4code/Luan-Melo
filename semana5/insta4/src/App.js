a40ca1a9ff96693859fe316cb51270e77dd6319c
Luan-Melo <luan_decruz19@hotmail.com>
2021-07-15 16:27:21 -0300

    insta-não-finalizado-parte1

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -10,30 +10,48 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    pessoas: [
+      {
+        nomeUsuario: 'Paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150',
+      },
+
+      {
+        nomeUsuario: 'Lucas',
+        fotoUsuario: 'https://picsum.photos/51/51',
+        fotoPost: 'https://picsum.photos/200/151',
+      }
+    ]
+  };
+
   render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
+   
+      return (
+        <MainContainer>
+          <Post
+            nomeUsuario={'paulinha'}
+            fotoUsuario={'https://picsum.photos/50/50'}
+            fotoPost={'https://picsum.photos/200/150'}
+          />
 
-        <Post
-          nomeUsuario={'luan'}
-          fotoUsuario={'https://picsum.photos/51/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
+          <Post
+            nomeUsuario={'luan'}
+            fotoUsuario={'https://picsum.photos/51/51'}
+            fotoPost={'https://picsum.photos/200/151'}
+          />
 
-        <Post
-          nomeUsuario={'fernanda'}
-          fotoUsuario={'https://picsum.photos/52/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+          <Post
+            nomeUsuario={'fernanda'}
+            fotoUsuario={'https://picsum.photos/52/52'}
+            fotoPost={'https://picsum.photos/200/152'}
+          />
 
-      </MainContainer>
-    );
-  }
+        </MainContainer>
+      );
+    }
 }
 
 export default App;
