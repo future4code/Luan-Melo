51f80a07972834f46baa2cf1f310214982f05b93
Luan-Melo <luan_decruz19@hotmail.com>
2021-07-14 23:18:58 -0300

    atividade18-finalizada

--- semana5/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from "styled-components";
+
+const ContainerCard = styled.div `
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 10px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+`
+const ImagemIcon = styled.img`
+    width: 90px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
+        <ContainerCard>
+            <ImagemIcon src={ props.imagem } />
             <div>
                 <h4>{ props.nome }</h4>
                 <p>{ props.descricao }</p>
             </div>
-        </div>
+        </ContainerCard>
     )
 }
 
--- semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import './CardPequeno.css'
+import styled from "styled-components";
+
+const ContainerLittleCard = styled.div `
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 0 10px;
+    margin-bottom: 10px;
+    height: 100px;
+`
+
+const LittleImg = styled.img `
+    height: 40px;
+    width: 70px;
+    border-radius: 50%;
+`
+
+const Paragraph = styled.p `
+    margin: 2px;
+`
 
 function CardPequeno(props) {
     return (
-        <div className="littlecard-container">
-            <img src={ props.icon } />
-            <strong><p>{ props.tipo  }</p></strong>
-            <p>{ props.informacao }</p>
-        </div>
+        <ContainerLittleCard>
+            <LittleImg src={ props.icon } />
+            <strong><Paragraph>{ props.tipo  }</Paragraph></strong>
+            <Paragraph>{ props.informacao }</Paragraph>
+        </ContainerLittleCard>
     )
 }
 
