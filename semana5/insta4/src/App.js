f46115674fd3abec81377a33739f3e30250e9ac8
Luan-Melo <luan_decruz19@hotmail.com>
2021-07-15 22:22:12 -0300

    finalizado

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -9,6 +9,27 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const InputContainer = styled.input`
+  margin: 5px;
+  padding: 10px;
+  text-align: center;
+
+`
+
+const ButtonContainer = styled.button`
+  margin: 5px;
+  padding: 10px;
+  cursor: pointer;
+  outline: 0;
+  border: none;
+  background-color: green;
+  color: white;
+
+  :hover {
+    background-color: #7bd66d;
+  }
+`
+
 class App extends React.Component {
 
   state = {
@@ -29,14 +50,67 @@ class App extends React.Component {
         nomeUsuario: 'Luan',
         fotoUsuario: 'https://picsum.photos/52/52',
         fotoPost: 'https://picsum.photos/200/152',
-      }
-    ]
+      },
+    ],
+    valorNome: "",
+    valorInserirUrl: "",
+    valorUrlPost: ""
   };
 
+  onChangeNome = (event) => {
+    this.setState({ valorNome: event.target.value })
+  }
+
+  onChangeImagemPerfil = (event) => {
+    this.setState({ valorInserirUrl: event.target.value })
+  }
+
+  onChangeImagePost = (event) => {
+    this.setState({ valorUrlPost: event.target.value })
+  }
+
+  addNewPeople = () => {
+    if (this.state.valorNome === ""){
+      return alert('O campo Nome está vazio')
+    }else if (this.state.valorInserirUrl === ""){
+      return alert('O campo da url do perfil, está vazio')
+    }else if (this.state.valorUrlPost === ""){
+      return alert('O campo da url do post está vazio')
+    }else {
+      alert('Cadastro do novo usuário feito com Sucesso!')
+    }
+
+    const newPeople = {
+      nomeUsuario: this.state.valorNome,
+      fotoUsuario: this.state.valorInserirUrl,
+      fotoPost: this.state.valorUrlPost
+    };
+
+    const newPeoples = [...this.state.pessoas, newPeople];
+    this.setState({ pessoas: newPeoples });
+  }
+
   render() {
-    // const {nomeUsuario, fotoUsuario, fotoPost} = this.state.pessoas[0]
         return (
         <MainContainer>
+            <InputContainer
+              value={this.state.valorNome}
+              onChange={this.onChangeNome}
+              placeholder={"Nome"}
+            />
+            <InputContainer
+              value={this.state.valorInserirUrl}
+              onChange={this.onChangeImagemPerfil}
+              placeholder={"Url Perfil"}
+            />
+
+            <InputContainer
+              value={this.state.valorUrlPost}
+              onChange={this.onChangeImagePost}
+              placeholder={"Url Post"}
+            />
+
+            <ButtonContainer onClick= {this.addNewPeople}>Carregar</ButtonContainer>
             {this.state.pessoas.map(({nomeUsuario, fotoUsuario, fotoPost}) => {
               return (
                 <Post
@@ -46,7 +120,8 @@ class App extends React.Component {
                 />
               )
             }
-          )};
+          )}
+
         </MainContainer>
       );
     }
