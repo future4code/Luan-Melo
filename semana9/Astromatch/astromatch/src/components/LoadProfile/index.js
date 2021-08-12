af7dcff78de87a0284be0e17d3976faa169a5a81
Luan-Melo <luan_decruz19@hotmail.com>
2021-08-12 19:30:44 -0300

    finalizar

--- semana9/Astromatch/astromatch/src/components/LoadProfile/index.js
+++ semana9/Astromatch/astromatch/src/components/LoadProfile/index.js
@@ -13,47 +13,56 @@ import {
 const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:luan"
 
 const LoadProfile = () => {
-
     const [person, setPerson] = useState({})
+    const [like, setLike] = useState(false)
     const { id, name, photo, bio, age } = person
 
-    const personView = async () => {
-        const body = {
-            "id": id,
-            "choice": true
-        }
-
-        try {
-            const choosePerson = await axios.post(`${url}/choose-person`, body)
-            console.log(choosePerson)
-        } catch {
-            console.log('Error')
-        }
-    }
-
     useEffect(() => {
-        axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:luanmelo/person")
+        getPerson()
+    }, [])
+
+    const getPerson = async () => {
+        await axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:luanmelo/person")
             .then((res) => {
                 setPerson(res.data.profile)
             })
             .catch((err) => {
                 console.log(err)
             })
-    }, [])
+    }
+
+    const personView = async (value) => {
+        const body = {
+            "id": id,
+            "choice": value
+        }
+
+        try {
+            const choosePerson = await axios.post(`${url}/choose-person`, body)
+            if (choosePerson.data.isMatch) {
+                setLike(true)
+            } else {
+                getPerson()
+            }
+        } catch {
+            console.log('Error')
+        }
+    }
 
     return (
         <ContainerGeral>
             <ContainerPerfil>
                 <MenuApp />
+
                 <p>{name}, {age}</p>
                 <Img src={photo} value={photo} alt={name} />
                 <p>{bio}</p>
                 <div>
-                    <button onClick={personView}>match</button>
-                    <button>delete</button>
+                    <button onClick={() => personView(true)}>match</button>
+                    <button onClick={() => personView(false)}>delete</button>
                 </div>
             </ContainerPerfil>
-
+            {like}
         </ContainerGeral>
     )
 }
