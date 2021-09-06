fdc1127e7b367468a0f769866cacba11b8a04471
Luan-Melo <luan_decruz19@hotmail.com>
2021-09-05 23:14:18 -0300

    finalzar-renderização

--- projetos/labeedit/src/pages/Home/Home.jsx
+++ projetos/labeedit/src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
+import { useHistory } from "react-router";
 import Header from "../Header/Header";
 
 const Home = () => {
-  const token = localStorage.getItem("token");
-  console.log(token);
-
+  const { push } = useHistory();
   return (
     <div>
       <Header />
       <h1>HOME</h1>
+      <button onClick={() => push("/login")}>Acessar</button>
     </div>
   );
 };
